09e4424049107f2a0fe7f3555fa5d50cb746a46d
ShotaL <52139226+ShotikoLabadze@users.noreply.github.com>
2022-11-03 23:14:21 +0400

    add counting logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>First Number: {firstNum}</p>
+
+      <button onClick={() => setFirstNum(firstNum + 1)}>First Button</button>
+      <br></br>
+      <p>Second Number: {secondNum}</p>
+      <button onClick={() => setSecondNum(secondNum + 1)}>Second Button</button>
+
+      <p>
+        {(() => {
+          if (firstNum > secondNum) {
+            return <p>First score is higher {firstNum}</p>;
+          } else if (firstNum == secondNum) {
+            return <p>Numbers are same</p>;
+          } else {
+            return <p>Second score is higher {secondNum}</p>;
+          }
+        })()}
+      </p>
     </div>
   );
 }
